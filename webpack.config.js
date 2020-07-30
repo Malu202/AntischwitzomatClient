8f20d2b128180796312e2179b0c965bcd69e240a
martin <poehlmann.martin@gmail.com>
2020-07-30 23:03:21 +0200

    fix base tag

--- webpack.config.js
+++ webpack.config.js
@@ -4,64 +4,70 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const path = require('path');
 
-module.exports = {
-    entry: {
-        main: './src/main.js'
-    },
-    devtool: "source-map",
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: [{
-                    loader: "html-loader",
-                    options: {
-                        minimize: true
-                    }
-                }]
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    { loader: "css-loader", options: {} },
-                    { loader: "postcss-loader", options: {} },
-                    {
-                        loader: "sass-loader", options: {
-                            implementation: require('sass'), sassOptions: {
-                                includePaths: ["node_modules"],
-                            },
+module.exports = (env, argv) => {
+    const production = argv.mode == "production";
+    const isGithubPages = env && env.githubpages;
+
+    const base = isGithubPages ? "/AntischwitzomatClient/" : "/";
+    return {
+        entry: {
+            main: './src/main.js'
+        },
+        devtool: "source-map",
+        module: {
+            rules: [
+                {
+                    test: /\.html$/,
+                    use: [{
+                        loader: "html-loader",
+                        options: {
+                            minimize: true
                         }
-                    }
-                ]
-            }
+                    }]
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        { loader: "css-loader", options: {} },
+                        { loader: "postcss-loader", options: {} },
+                        {
+                            loader: "sass-loader", options: {
+                                implementation: require('sass'), sassOptions: {
+                                    includePaths: ["node_modules"],
+                                },
+                            }
+                        }
+                    ]
+                }
+            ],
+        },
+        resolve: {
+            extensions: ['.js'],
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[contenthash].bundle.js',
+            publicPath: '/'
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                base: base, title: "Antischwitzomat",
+                template: 'src/index.html'
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].[contenthash].css',
+                chunkFilename: '[id].[contenthash].css',
+            }),
+            new CleanWebpackPlugin()
         ],
-    },
-    resolve: {
-        extensions: ['.js'],
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[contenthash].bundle.js',
-        publicPath: '/'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            base: "/", title: "Antischwitzomat",
-            template: 'src/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css',
-            chunkFilename: '[id].[contenthash].css',
-        }),
-        new CleanWebpackPlugin()
-    ],
-    mode: "development",
-    devServer: {
-        compress: true,
-        port: 9000,
-        historyApiFallback: {
-            index: "/"
+        mode: "development",
+        devServer: {
+            compress: true,
+            port: 9000,
+            historyApiFallback: {
+                index: "/"
+            }
         }
-    }
-};
+    };
+}
