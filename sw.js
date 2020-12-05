6627d58aa37a831180d9bcd8f3a41e0c37e7fe50
Malu202 <29000299+Malu202@users.noreply.github.com>
2020-12-05 16:29:12 +0000

    Deploying to gh-pages from  @ 22940b01270bdeeb0e359eb1da9e753ea59dab82 🚀

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ var serviceWorkerOption = {
     "/AntischwitzomatClient/favicons/safari-pinned-tab.svg",
     "/AntischwitzomatClient/site.webmanifest",
     "/AntischwitzomatClient/main.1c6171403a81ce022190.css",
-    "/AntischwitzomatClient/772a36082f0b5566e021.bundle.js",
+    "/AntischwitzomatClient/ea56458e576367bf1e47.bundle.js",
     "/AntischwitzomatClient/1.9951564a3fd2c1b82bed.bundle.js",
     "/AntischwitzomatClient/index.html"
   ]
